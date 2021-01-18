0624d62081abb47c07614b23c1827896ad4365de
Claire Cheek <claireccheek@gmail.com>
2021-01-18 14:49:04 -0800

    server communicating with host 3000

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const { response } = require("express")
 const express = require("express")
+const fs = require("fs")
 
 const server = express()
 
+PORT = 3000
+
+server.get("/", (req, res) => {
+    res.send("yo")
+})
+
+
+
+
+server.listen(PORT, function(){
+    console.log(`run run run ${PORT}`);
+})
